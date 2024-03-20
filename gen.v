fdbf4b1ce6f0dc049e767ad3df164e98cbdb8fe0
VLSIX411-Winter2024-6 <albchi@gmail.com>
2024-03-19 17:16:47 -0700

    added assertion

--- gen.v
+++ gen.v
@@ -6,8 +6,8 @@ reg clk;
 reg rst;
 
 typedef enum {RST, INIT, ADD, SUB, MULT, DIV, REM, HLT} CMD_TYPE ;
-reg vld [15:0] = '{0,0,1,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0};
-reg rdy [15:0] = '{0,0,1,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0};
+reg vld [15:0] = '{0,0,1,0,0,0,0,0,1,0, 1, 0, 0, 0, 0, 0};
+reg rdy [15:0] = '{0,0,0,0,1,0,0,0,0,0, 0, 0, 0, 0, 0, 0};
 CMD_TYPE cmd [15:0] = '{ADD,SUB,INIT,MULT,DIV,INIT,ADD,REM,INIT,HLT,DIV,RST,SUB,RST,ADD,REM};
 reg [63:0] opd1 [15:0] = '{0,0,1,0,0,1,0,1,0,0, 0, 0, 0, 0, 0, 0};
 reg [63:0] opd2 [15:0] = '{0,0,1,0,1,0,0,0,0,0, 0, 0, 0, 0, 0, 0};
@@ -15,6 +15,7 @@ reg done [15:0] = '{0,0,1,0,1,1,1,0,0,0, 0, 0, 1, 1, 0, 0};
 CMD_TYPE done_cmd [15:0] = '{RST, INIT, HLT, ADD,SUB,INIT,MULT,DIV,INIT,ADD,REM,INIT,HLT,DIV,RST,SUB};
 reg [3:0] idx;
 
+reg vld_o;
 reg rdy_o;
 reg [2:0] cmd_o;
 reg [63:0] opd1_o;
@@ -37,6 +38,7 @@ always
    
 always @(posedge clk)  begin
    if (rst == 1)  begin
+      vld_o <= 0;
       rdy_o <= 0;
       cmd_o <= 0;
       opd1_o <= 0;
@@ -46,6 +48,7 @@ always @(posedge clk)  begin
    end
    else begin 
       rdy_o <= rdy[idx];
+      vld_o <= vld[idx];
       cmd_o <= cmd[idx];
       opd1_o <= opd1[idx];
       opd2_o <= opd2[idx];
@@ -57,5 +60,11 @@ end
 
 
 
+property p_vld_rdy;
+   @(posedge clk)
+      vld_o |-> ##1 (!vld_o throughout rdy_o[->1]);
+endproperty
+
+a_vld_rdy : assert property (a_vld_rdy);
 
 endmodule
